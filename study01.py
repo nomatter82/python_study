25d8585b13744ceb02b0d2ba7ede5ce2e0b73bc7
nomatter82 <mirthful82@gmail.com>
2022-10-21 17:54:52 +0900

    파이썬 표준 타입

---
+++ study01.py
@@ -0,0 +1,68 @@
+
+
+# my_tuple = {1, 2, 3, 4}
+
+# my_tuple = [1, 2, 3]
+
+# print(my_tuple)
+
+# for item in my_tuple:
+#     print(item)
+
+# my_dict = {"name" : "nomatter82", "age" : 22}
+
+# for key,value in my_dict.items():
+    # print(f"{key} 은/는 {value}")
+
+# my_set = {1,2,3,4,1,1,1,1}
+# my_set = set({1,2,3,4,1,1,1,1})
+
+# print(my_set)
+
+# for item in my_set:
+    # print(item) 
+
+
+
+# print(True or False)
+
+# result = not (1 != 2)
+
+# print(result)
+
+# #x = 1
+# #While x < 10:
+# #    print("가나다")
+# #    x = x + 1
+     
+
+# for item in [1,2,3,4]:
+#    print(item)
+
+
+# def sum_two_num(n1, n2):
+#     return(n1+ n2)
+
+# sum_num = sum_two_num(1,2)
+# sum_num = 3
+
+# print(sum_num)
+
+# def check_data():
+#     data = int(input())
+    
+
+#     if data == 1:
+#         print("1이다")
+#     elif data == 2:
+#         print("2다")
+#     elif data == 3:
+#         print("3이다")
+#     elif data == 4:
+#         print("4다")
+#     else data == 5:
+#         print("모르겠다")
+        
+   
+# while True:
+#     check_data
